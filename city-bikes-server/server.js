8ecff2cbf25fcaf0a1af906abb948371e41f8c9b
ammarnaqvi <ammar.naqvi@apimatic.io>
2018-09-05 23:56:59 +0500

    api progress

--- city-bikes-server/server.js
+++ city-bikes-server/server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.get('/api/bike-stations', (req, res) => {
-  res.send({ express: 'Have a bike' });
+
+app.get("/api/bike-stations", async (req, res) => {
+  const city = req.query.city;
+  let networksResponse = await fetch(
+    "https://api.citybik.es/v2/networks?fields=location,id"
+  );
+  let json = await networksResponse.json();
+  let networks = json.networks;
+
+  let cityNetworks = networks.filter((network) => {
+    return network.location.city == city;
+  });
+
+  res.send(cityNetworks);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
